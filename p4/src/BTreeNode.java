caf9aa7c0825432867ad2c590ca769a49ad283c2
James Brooks <jamesbrooks1@u.boisestate.edu>
2018-03-24 09:57:50 -0600

    Some basic class setup

--- p4/src/BTreeNode.java
+++ p4/src/BTreeNode.java
@@ -1,4 +1,36 @@
+import java.util.LinkedList;
 
+/**
+ * @author James Brooks
+ *
+ */
 public class BTreeNode {
+	private int objectCount;
+	private int location; //byte offset of this node in file
+	private boolean isLeaf;
+	private LinkedList<TreeObject> objects; //Maybe array is better???
+	private int parent; //pointer to byte offset location of parent in file
+	private int[] child; //pointers to byte offset locations of children in file
+	
+	
+	public BTreeNode(LinkedList<TreeObject> objects) {
+		this.objects = objects;
+	}
+	
+	public int getObjectCount() {
+		return objectCount;
+	}
+
+	public int getLocation() {
+		return location;
+	}
+	
+	public boolean isLeaf() {
+		return isLeaf;
+	}
+	
+	public int getParent() {
+		return parent;
+	}
 
 }
--- p4/src/TreeObject.java
+++ p4/src/TreeObject.java
@@ -1,4 +1,30 @@
 
+/**
+ * @author James Brooks
+ *
+ */
 public class TreeObject {
+	private long data;
+	private int frequency;
+	
+	public TreeObject(long data) {
+		this.data = data;
+		frequency = 1;
+	}
+	
+	public long getData() {
+		return data;
+	}
 
+	public int getFrequency() {
+		return frequency;
+	}
+	
+	public void incrementFrequency() {
+		frequency++;
+	}
+
+	public void decrementFrequency() {
+		frequency--;
+	}
 }
